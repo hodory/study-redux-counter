e8329ac5cfd15bbe3b35346cb008cb89daa8d105
HoYoungKim <mo02@outlook.kr>
2017-09-10 22:35:09 +0900

    1st chapter

---
+++ src/actions/index.js
@@ -0,0 +1,20 @@
+/*
+    action 객체를 만드는 액션 생성자들을 선언(action creators)
+    여기서 ()=> ({}) 은, function() { return{} } 와 동일한 의미
+    scope 이슈와 관계 없이 편의상 사용
+*/
+import * as types from './ActionTypes';
+
+export const increment = () => ({
+    type: types.INCREMENT
+});
+
+export const decrement = () => ({
+    type: types.DECREMENT
+});
+
+// 다른 액션 생성자들과 달리, 파라미터를 갖고 있다.
+export const setColor = (color) => ({
+    type: types.SET_COLOR,
+    color
+});
